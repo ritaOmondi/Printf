75e8e1744344281280a6baa1991a74720566a0b6
ritaOmondi <ritaOmondi>
2023-09-18 03:44:04 -0700

    editing binary conversion

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**void print_binary(unsigned int num); **/
+void print_binary(unsigned int num);
 /**
  * _printf - Function that prints to stdou format
  * @format : Input character
@@ -17,13 +17,13 @@ while (*format != '\0')
 if (*format == '%')
 {
 format++;
-/**if (*format == 'b')
+if (*format == 'b')
 {
 unsigned int num = va_arg(args, unsigned int);
 print_binary(num);
 count += calculate_number_of_digits(num, 2);
-} **/
-if (*format == 'c')
+}
+else if (*format == 'c')
 {
 char c = (char)va_arg(args, int);
 _putchar(c);
